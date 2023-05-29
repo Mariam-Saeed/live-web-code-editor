c3be03694abbc9a62a5e4a760371c7accb8ab18c
Mariam Saeed <mariamsaeed750@gmail.com>
2023-05-29 21:05:57 +0300

    added auto-save and auto-load functionality

--- js/app.js
+++ js/app.js
@@ -1,37 +1,59 @@
-document.getElementById("htmlCode").value="<div>\n\n</div>";
-document.getElementById("cssCode").value="<style>\n\n</style>";
-document.getElementById("jsCode").value="<script>\n\n</script>";
+let html = document.getElementById('htmlCode');
+let css = document.getElementById('cssCode');
+let js = document.getElementById('jsCode');
 
-function showPreview(){
-    var htmlCode = document.getElementById("htmlCode").value;
-    var cssCode = ""+document.getElementById("cssCode").value+"";
-    var jsCode = ""+document.getElementById("jsCode").value+"";
-    var frame = document.getElementById("preview-window").contentWindow.document;
-    frame.open();
-    frame.write(htmlCode+cssCode+jsCode);
-    frame.close();
+const autoLoad = () => {
+  if (localStorage.getItem('html')) html.value = localStorage.getItem('html');
+  else html.value = '<div>\n\n</div>';
+
+  if (localStorage.getItem('css')) css.value = localStorage.getItem('css');
+  else css.value = '<style>\n\n</style>';
+
+  if (localStorage.getItem('js')) js.value = localStorage.getItem('js');
+  else js.value = '<script>\n\n</script>';
+};
+
+const autoSave = (html, css, js) => {
+  localStorage.setItem('html', html);
+  localStorage.setItem('css', css);
+  localStorage.setItem('js', js);
+};
+
+function showPreview() {
+  var htmlCode = html.value;
+  var cssCode = '' + css.value + '';
+  var jsCode = '' + js.value + '';
+  var frame = document.getElementById('preview-window').contentWindow.document;
+
+  autoSave(htmlCode, cssCode, jsCode);
+
+  frame.open();
+  frame.write(htmlCode + cssCode + jsCode);
+  frame.close();
 }
 
-function show(x){
-    document.getElementById("html").style.display="none";
-    document.getElementById("css").style.display="none";
-    document.getElementById("js").style.display="none";
-    document.getElementById("result").style.display="none";
-    document.getElementById(x).style.display="block";
+function show(x) {
+  document.getElementById('html').style.display = 'none';
+  document.getElementById('css').style.display = 'none';
+  document.getElementById('js').style.display = 'none';
+  document.getElementById('result').style.display = 'none';
+  document.getElementById(x).style.display = 'block';
 }
 
-function show_all(){
-    if(window.innerWidth>=992)
-    {
-        document.getElementById("html").style.display="block";
-        document.getElementById("css").style.display="block";
-        document.getElementById("js").style.display="block";
-        document.getElementById("result").style.display="block";
-    }
-    if(window.innerWidth<992 && document.getElementById("html").style.display=="block")
-    {
-        document.getElementById("css").style.display="none";
-        document.getElementById("js").style.display="none";
-        document.getElementById("result").style.display="none";
-    }
+// for mobile
+function show_all() {
+  if (window.innerWidth >= 992) {
+    document.getElementById('html').style.display = 'block';
+    document.getElementById('css').style.display = 'block';
+    document.getElementById('js').style.display = 'block';
+    document.getElementById('result').style.display = 'block';
+  }
+  if (
+    window.innerWidth < 992 &&
+    document.getElementById('html').style.display == 'block'
+  ) {
+    document.getElementById('css').style.display = 'none';
+    document.getElementById('js').style.display = 'none';
+    document.getElementById('result').style.display = 'none';
+  }
 }
